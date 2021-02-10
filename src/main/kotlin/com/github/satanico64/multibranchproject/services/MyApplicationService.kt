d7c5eba60d04a3961b4049bee19f98558ad7ecb0
GitHub Action <action@github.com>
2021-02-10 09:53:06 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/satanico64/multibranchproject/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.satanico64.multibranchproject.services
+
+import com.github.satanico64.multibranchproject.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/satanico64/multibranchproject/services/MyProjectService.kt
+++ src/main/kotlin/com/github/satanico64/multibranchproject/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.satanico64.multibranchproject.services
 
+import com.github.satanico64.multibranchproject.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
